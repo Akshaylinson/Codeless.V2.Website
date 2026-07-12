0d92eb2998d386141805afab4321ba5b81249f12
Akshaylinson <akshaylinson24@gmail.com>
2026-07-12 19:14:34 +0530

    updated all the pages

--- remove_team.py
+++ remove_team.py
@@ -1,9 +1,9 @@
 import os, re, glob
 
-# The exact "Our team" col block to remove from the megamenu
-TEAM_BLOCK = re.compile(
+# Remove the Project details col block from the megamenu
+PROJECT_DETAILS_BLOCK = re.compile(
     r'\s*<div class="col-xl-4 col-lg-4">\s*'
-    r'<a class="iconbox_block_2" href="team\.html">.*?</a>\s*'
+    r'<a class="iconbox_block_2" href="project-details\.html">.*?</a>\s*'
     r'</div>',
     re.DOTALL
 )
@@ -12,13 +12,10 @@ html_files = glob.glob('*.html')
 for fname in html_files:
     with open(fname, 'r', encoding='utf-8') as f:
         content = f.read()
-    if 'team.html' in content and 'iconbox_title' in content:
-        new_content = TEAM_BLOCK.sub('', content)
-        if new_content != content:
-            with open(fname, 'w', encoding='utf-8') as f:
-                f.write(new_content)
-            print(f'Updated: {fname}')
-        else:
-            print(f'No match found: {fname}')
+    new_content = PROJECT_DETAILS_BLOCK.sub('', content)
+    if new_content != content:
+        with open(fname, 'w', encoding='utf-8') as f:
+            f.write(new_content)
+        print(f'Updated: {fname}')
     else:
-        print(f'Skipped: {fname}')
+        print(f'No match: {fname}')
